97ba4bb12d7816d43d8ee85e36c0194936d5bcb2
kp1673 <kpermth@gmail.com>
2023-05-23 18:21:35 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Main from './Main';
+import Nav from './Nav';
+import { ReactComponent as Logo } from './Logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header>
+        <Logo/>
+        <Nav/>
+      </Header>
+      <Main>
+
+      </Main>
+      <Footer>
+
+      </Footer>
+    </>
   );
 }
 
